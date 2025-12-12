f25f0fadb42973c81ad5472544bbaf6a471e68ac
sawariyaseth <radhakrishna2kr@gmail.com>
2025-12-12 15:59:30 +0000

    updated multiple options of console.table

--- .github/01 basics/01_variables.js
+++ .github/01 basics/01_variables.js
@@ -9,4 +9,54 @@ accountpassword='harshit1234';
 accountcity='delhi';
 // accountID='0987654321'; // This will cause an error because accountID is a constant
 
-console.log('Account ID:', accountID,accountEmail,accountpassword,accountcity,accountState,accountEmail);
+
+console.log(`
+  Account ID: ${accountID}
+  Email: ${accountEmail}
+  Password: ${accountpassword}
+  City: ${accountcity}
+  State: ${accountState}
+`);
+
+/*Option 1: Array of arrays
+console.table(['Account ID:', accountID],['accountemail',accountEmail],['accountpassword' ,accountpassword],['accountcity',accountcity],['accountstate',accountState]);
+
+Option 2: : Using Object.assign() with an empty object
+console.table(Object.assign({}, {
+  accountID,
+  accountEmail,
+  accountpassword,
+  accountcity,
+  accountState
+}));
+
+*/
+
+/*Option 3: Single object (cleaner)
+console.table({
+  'Account ID': accountID,
+  'Email': accountEmail,
+  'Password': accountpassword,
+  'City': accountcity,
+  'State': accountState
+});
+*/
+/*Option 4: Array of objects
+console.table([{
+  'Account ID': accountID,
+  'Email': accountEmail,
+  'Password': accountpassword,
+  'City': accountcity,
+  'State': accountState
+}]);
+*/
+
+/*Option 5: Formatted string output
+console.log(`
+  Account ID: ${accountID}
+  Email: ${accountEmail}
+  Password: ${accountpassword}
+  City: ${accountcity}
+  State: ${accountState}
+`);
+*/ 
